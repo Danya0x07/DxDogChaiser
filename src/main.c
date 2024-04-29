934343e0b49d02898d5f60382102b1e1b68f7fd3
Danya0x07 <danya0x07@vk.com>
2024-04-29 15:30:53 +0300

    Timer1 working

--- src/main.c
+++ src/main.c
@@ -74,6 +74,50 @@ void CAN_SendChar(char c)
 
 CanRxMsg rxmsg;
 
+void Tim1_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure={0};
+    TIM_OCInitTypeDef TIM_OCInitStructure={0};
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure={0};
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    TIM_TimeBaseInitStructure.TIM_Period = 160;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 5;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_CenterAligned3;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
+
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+    TIM_OCInitStructure.TIM_Pulse = 40;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+    TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+    TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Reset;
+    TIM_OC2Init(TIM1, &TIM_OCInitStructure);
+
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+    TIM_OCInitStructure.TIM_Pulse = 120;
+    TIM_OC3Init(TIM1, &TIM_OCInitStructure);
+
+    TIM_CtrlPWMOutputs(TIM1, ENABLE);
+    TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Disable);
+    TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Disable);
+    TIM_ARRPreloadConfig(TIM1, ENABLE);
+    TIM_Cmd(TIM1, ENABLE);
+}
+
 int main(void)
 {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
@@ -82,7 +126,7 @@ int main(void)
     Delay_Init();
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 | RCC_APB1Periph_CAN1, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOA | RCC_APB2Periph_TIM1, ENABLE);
 
     Delay_Ms(100);
 
@@ -94,6 +138,7 @@ int main(void)
     ISP_SendString("Begining\n");
     printf("Clock is: %ld\n", SystemCoreClock);
     RS485_SendString("Begining\n");
+    Tim1_Init();
 
     for (;;) {
 	if (ISP_LineReceived()) {
@@ -103,6 +148,14 @@ int main(void)
 	    RS485_SendChar('\n');
 
 	    CAN_SendChar(line[0]);
+
+	    if (line[0] >= '0' && line[0] <= '9' && line[0] >= '0' && line[0] <= '9') {
+		uint16_t pulse = (line[0] - '0') * 10 + (line[1] - '0');
+
+		pulse = (pulse * 160 / 10 + 5) / 10;
+		TIM_SetCompare2(TIM1, pulse);
+		TIM_SetCompare3(TIM1, 160 - pulse);
+	    }
 	}
 	if (RS485_LineReceived()) {
 	    char line[64];
